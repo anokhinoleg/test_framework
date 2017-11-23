8a2590665fe910e41b64c39b34527b701ee42e01
Oleg Anokhin <olegyurievich1991@gmail.com>
2017-11-23 08:56:36 +0200

    templating

--- src/app.php
+++ src/app.php
@@ -1,12 +1,32 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
+function is_leap_year($year = null) {
+    if (null === $year) {
+        $year = date('Y');
+    }
+    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+}
+
 $routes = new RouteCollection();
+$routes->add(
+    'leap_year',
+    new Route(
+        '/is_leap_year/{year}',
+        array(
+            'year' => null,
+            '_controller' => function ($request) {
+                if (is_leap_year($request->attributes->get('year'))) {
+                    return new Response('Yep, this is a leap year!');
+                }
 
-$routes->add('bye', new Route('/bye'));
-$routes->add('index', new Route('/{name}', array('name' => 'World')));
-
+                return new Response('Nope, this is not a leap year.');
+            }
+        )
+    )
+);
 
 return $routes;
--- web/front.php
+++ web/front.php
@@ -5,9 +5,16 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\Generator\UrlGenerator;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
+
+function render_template($request)
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+    return new Response(ob_get_clean());
+}
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__ . '/../src/app.php';
@@ -15,20 +22,9 @@ $context = new RequestContext();
 $context->fromRequest($request);
 $matcher = new UrlMatcher($routes, $context);
 
-$generator = new UrlGenerator($routes, $context);
-$route = $generator->generate('bye', array(
-    'name' => 'john_conner'
-));
-
-
-
 try {
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-//    var_dump($request->getPathInfo());
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-
-    $response = new Response(ob_get_clean());
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $response = call_user_func($request->attributes->get('_controller'), $request);
 } catch (ResourceNotFoundException $e) {
     $response = new Response('Not Found', 404);
 } catch (Exception $e) {
